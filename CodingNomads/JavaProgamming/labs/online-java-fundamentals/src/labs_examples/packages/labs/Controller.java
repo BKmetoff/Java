bb905b457c58c39b03944500dafccee3f6c4fb37
Boyan Kmetov <bkmetoff@gmail.com>
2021-06-17 15:21:36 +0200

    packages labs

---
+++ CodingNomads/JavaProgamming/labs/online-java-fundamentals/src/labs_examples/packages/labs/Controller.java
@@ -0,0 +1,11 @@
+package labs_examples.packages.labs;
+import labs_examples.packages.labs.package_01.*;
+
+public class Controller {
+
+    public static void main(String[] args) {
+        Package_01_Class_01 package_01_class_01 = new Package_01_Class_01();
+        System.out.println(package_01_class_01.publicMethod());
+
+    }
+}
---
+++ CodingNomads/JavaProgamming/labs/online-java-fundamentals/src/labs_examples/packages/labs/package_01/Package_01_Class_02.java
@@ -0,0 +1,11 @@
+package labs_examples.packages.labs.package_01;
+
+public class Package_01_Class_02 {
+    public String publicMethod() {
+        return "returned from publicMethod in Package_01_Class";
+    }
+
+    protected String protectedMethod() {
+        return "returned from protectedMethod in Package_01_Class";
+    }
+}
