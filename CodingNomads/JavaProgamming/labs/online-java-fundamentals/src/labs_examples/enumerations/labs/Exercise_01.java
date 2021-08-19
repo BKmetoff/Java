43f38ebb2967fc95b1f1a0019446195710b7106b
Boyan Kmetov <bkmetoff@gmail.com>
2021-08-19 15:25:56 +0200

    labs enum

--- CodingNomads/JavaProgamming/labs/online-java-fundamentals/src/labs_examples/enumerations/labs/Exercise_01.java
+++ CodingNomads/JavaProgamming/labs/online-java-fundamentals/src/labs_examples/enumerations/labs/Exercise_01.java
@@ -3,8 +3,41 @@ package labs_examples.enumerations.labs;
 /**
  * Enumerations Exercise 1:
  *
- *      1) Create an enumeration. Use a controller class to demonstrate the use of the enum from a seperate class.
- *      2) Create a parameterized enumeration that passes one or more values to it's constructor. Demonstrate the use
- *      of this enum from a seperate class.
+ *      1) Create an enumeration. Use a controller class to demonstrate the use of the enum from a separate class.
+ *      2) Create a parameterized enumeration that passes one or more values to its constructor. Demonstrate the use
+ *      of this enum from a separate class.
  */
 
+enum CardFaces {
+    JACK(10, "Jack"),
+    QUEEN(10, "Queen"),
+    KING(10, "King"),
+    ACE(1, "Ace");
+
+    private int points;
+    private String face;
+
+    CardFaces(int points, String face) {
+        this.points = points;
+        this.face = face;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getFace() {
+        return face;
+    }
+}
+
+class Exercise_01 {
+    public static void main(String[] args) {
+        CardFaces ace = CardFaces.ACE;
+        CardFaces king = CardFaces.KING;
+
+        System.out.println(ace.getPoints() + ace.getFace());
+        System.out.println(king.getPoints() + king.getFace());
+
+    }
+}
